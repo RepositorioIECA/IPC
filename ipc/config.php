ac51d7b4ebdd86990a1ef493c841cc1c9fe4d9a7
RepositorioIECA <99725187+RepositorioIECA@users.noreply.github.com>
2022-02-17 13:04:39 +0100

    Add files via upload

---
+++ ipc/config.php
@@ -0,0 +1,8 @@
+<?php
+$color       = ESTAD;
+$tipo        = PAG_PRODUCTO;
+$titulo      = "Índice de Precios de Consumo";
+$titulo_en   = "Consumer Price Index";
+$descripcion = "El Índice de Precios de Consumo mide la evolución del nivel de precios de los bienes y servicios de consumo adquiridos por los hogares.";
+$caminomigas = caminoMigas('est','condiciones');
+?>
